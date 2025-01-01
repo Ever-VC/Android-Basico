87e5b6937525751b574c0dd035c3ea9f3f99fd60
Ever-VC <ever.vasquezc27@gmail.com>
2025-01-01 16:37:38 -0600

    ✨ New Feature: Permitiendo que pueda realizar dos operaciones, seleccionando una a la vez (suma y resta)

--- app/src/main/java/com/example/proyecto001/MainActivity.java
+++ app/src/main/java/com/example/proyecto001/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.proyecto001;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText et1, et2;
     private TextView tvResultado;
+    private RadioButton rbtnSuma, rbtnResta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         et1 = findViewById(R.id.et1);
         et2 = findViewById(R.id.et2);
         tvResultado = findViewById(R.id.tvResultado);
+        rbtnSuma = findViewById(R.id.rbtnSuma);
+        rbtnResta = findViewById(R.id.rbtnResta);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -35,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void calcularSuma(View view) {
+    public void calcular(View view) {
         // Capturando los valores ingresados por el usuario en los EditText
         String s1 = et1.getText().toString();
         String s2 = et2.getText().toString();
@@ -50,10 +54,32 @@ public class MainActivity extends AppCompatActivity {
         int n1 = Integer.parseInt(s1);
         int n2 = Integer.parseInt(s2);
 
+        // Variable que almacenará el resultado de la suma
+        int resultado = 0;
+
+        // Validando si el RadioButton de la suma está seleccionado o no
+        if (rbtnSuma.isChecked()) {
+            sumar(n1, n2); // Llamando al método sumar
+        } else if (rbtnResta.isChecked()) {
+            restar(n1, n2); // Llamando al método restar
+        } else {
+            Toast.makeText(this, "Por favor, seleccione una operación", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void sumar(int n1, int n2) {
         // Realizando la suma
         int suma = n1 + n2;
 
         // Mostrando el resultado en el TextView
-        tvResultado.setText("Resultado: " + suma);
+        tvResultado.setText("El resultado es: " + suma);
+    }
+
+    private void restar(int n1, int n2) {
+        // Realizando la resta
+        int resta = n1 - n2;
+
+        // Mostrando el resultado en el TextView
+        tvResultado.setText("El resultado es: " + resta);
     }
 }
